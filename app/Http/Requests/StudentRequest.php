e8a7da56b28f9f35d3c23cc86def5af9bc12c58e
Tomi1819 <toshevtomi@gmail.com>
2023-01-18 00:24:20 +0200

    Initial commit

---
+++ app/Http/Requests/StudentRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'facultyNumber' => 'required',
+            'studentName' => 'required|min:2|max:255',
+            'teacherName' => 'required|min:2|max:255',
+            'course' => 'required|min:1|max:255',
+            'speciality' => 'required|min:2|max:255',
+            'thesis' => 'required|min:2|max:255'
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2023_01_17_210930_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('facultyNumber')->unique();
+            $table->string('studentName');
+            $table->string('teacherName');
+            $table->string('course');
+            $table->string('speciality');
+            $table->string('thesis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
